8b3e553d6ae22c582d0efdfea4d36de146eeb3ce
ybhsos <ybhsos@gmail.com>
2022-11-13 23:18:59 +0900

    ComposeArticle 완성 커밋

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -21,15 +21,31 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleTheme{
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    ComposeArticleApp()
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+
+    ArticleCard(stringResource(R.string.title_jetpack_compose_tutorial),
+        stringResource(R.string.compose_short_desc),
+        stringResource(R.string.compose_long_desc),
+        painterResource(R.drawable.bg_compose_background),
+        Modifier)
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,7 +55,33 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+
+    Column() {
+
+            Image(
+                painter = imagePainter,
+                contentDescription = null,
+            )
+            Text(
+                text = title,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
+            )
+            Text(
+                text = shortDescription,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify
+            )
+            Text(
+                text = longDescription,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp),
+                textAlign = TextAlign.Justify
+            )
+
+    }
 }
 
 
